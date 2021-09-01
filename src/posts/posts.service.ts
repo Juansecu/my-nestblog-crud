a7ccc2a78275d028fa4fcbd5ed4d13ec29d5ad5e
Juansecu <juansecoder@hotmail.com>
2021-09-01 15:36:36 -0500

    Functionality for getPosts method from PostsService was setted

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { PostEntity } from './entities/post.entity';
 
 import { CreatePostDto, EditPostDto } from './dtos';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private readonly _POST_REPOSITORY: Repository<PostEntity>
+  ) {}
+
   createPost(createPostDto: CreatePostDto) {}
 
   deletePost(postId: number) {}
 
-  getPosts() {}
+  async getPosts(): Promise<PostEntity[]> {
+    return await this._POST_REPOSITORY.find();
+  }
 
   getPost(postId: number) {}
 
